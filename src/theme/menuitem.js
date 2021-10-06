9daa4d7e6f99f79ac21abb6db0faa172de26f5d0
soldi3rofwarz <josejehovanicastroperez@gmail.com>
2021-10-05 18:57:00 -0600

    terminando el header

--- src/theme/menuitem.js
+++ src/theme/menuitem.js
@@ -1,27 +1,13 @@
 export default [
     {
       id: 1,
-      title: "Home",
+      title: "Inicio",
       cName: "nav-link",
     },
     {
       id: 2,
-      title: "About",
+      title: "Historias",
       cName: "nav-link color",
     },
-    {
-      id: 3,
-      title: "Services",
-      cName: "nav-link",
-    },
-    {
-      id: 4,
-      title: "Contact",
-      cName: "nav-link",
-    },
-    {
-      id: 5,
-      title: "Feedback",
-      cName: "nav-link",
-    },
+    
   ];
